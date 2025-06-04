3ffc692a740cc234f3fd49e7e4475eb2e96a4f3a
ROO73D <73015074+ROO73D@users.noreply.github.com>
2025-06-04 14:45:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-python3 /root/Desktop/Turnstile-Solver/api_solver.py --headless True --browser_type chrome --host 0.0.0.
+mkdir -p /root/Desktop
+
+cd /root/Desktop || {
+    echo "Failed to change directory to /root/Desktop"
+    exit 1
+}
+
+git clone https://github.com/Theyka/Turnstile-Solver.git
+cd Turnstile-Solver || {
+    echo "Failed to change directory to Turnstile-Solver"
+    exit 1
+}
+
+pip3 install -r requirements.txt --break-system-packages
+
+python3 /root/Desktop/Turnstile-Solver/api_solver.py --headless True --browser_type chrome --host 0.0.0.0
