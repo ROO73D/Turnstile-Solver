f22141341a2c9b0665b4c46d56c29648503b6369
ROO73D <73015074+ROO73D@users.noreply.github.com>
2025-06-27 00:23:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,80 @@
 #!/bin/bash
 
-python3 api_solver.py --headless True --browser_type camoufox
+start_xrdp_services() {
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/xrdp-sesman.pid
+    rm -rf /var/run/xrdp/xrdp.pid
 
+    xrdp-sesman &
+    xrdp -n &
+
+    echo "Waiting for X server to be ready..."
+    for i in {1..20}; do
+        if pgrep Xorg >/dev/null; then
+            echo "Xorg is running."
+            return
+        fi
+        sleep 1
+    done
+
+    echo "Xorg not detected after timeout."
+}
+
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
+
+if id "root" &>/dev/null; then
+    echo "root:root" | chpasswd || {
+        echo "Failed to update password."
+        exit 1
+    }
+else
+    if ! getent group root >/dev/null; then
+        addgroup root
+    fi
+
+    useradd -m -s /bin/bash -g root root || {
+        echo "Failed to create user."
+        exit 1
+    }
+    echo "root:root" | chpasswd || {
+        echo "Failed to set password."
+        exit 1
+    }
+    usermod -aG sudo root || {
+        echo "Failed to add user to sudo."
+        exit 1
+    }
+fi
+
+if [ -n "$TZ" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+    echo $TZ >/etc/timezone
+fi
+
+mkdir -p /root/Desktop
+
+cd /root/Desktop || {
+    echo "Failed to change directory to /root/Desktop"
+    exit 1
+}
+
+git clone https://github.com/Theyka/Turnstile-Solver.git
+cd Turnstile-Solver || {
+    echo "Failed to change directory to Turnstile-Solver"
+    exit 1
+}
+
+pip3 install -r requirements.txt --break-system-packages
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
+
+if [ "$RUN_API_SOLVER" = "true" ]; then
+    echo "Starting API solver in headful mode..."
+    xvfb-run -a python3 /root/Desktop/Turnstile-Solver/api_solver.py --browser_type chrome --host 0.0.0.0 --headless True --useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
+fi
