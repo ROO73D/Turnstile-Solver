c76a70c3acbdfc71cd7e9f9cb0e6fcc40ec07871
ROO73D <73015074+ROO73D@users.noreply.github.com>
2025-06-27 00:36:36 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-
 mkdir -p /root/Desktop
-
-cd /root/Desktop || {
-    echo "Failed to change directory to /root/Desktop"
-    exit 1
-}
+cd /root/Desktop || exit 1
 
 git clone https://github.com/Theyka/Turnstile-Solver.git
-cd Turnstile-Solver || {
-    echo "Failed to change directory to Turnstile-Solver"
-    exit 1
-}
+cd Turnstile-Solver || exit 1
 
-python3 -m pip install -r requirements.txt 
+apt update && apt install -y python3-full python3.12-venv
 
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
 
-
-python3 /root/Desktop/Turnstile-Solver/api_solver.py --browser_type chrome --host 0.0.0.0 --headless True --useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
-
+python3 api_solver.py --browser_type chrome --host 0.0.0.0 --headless True --useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
